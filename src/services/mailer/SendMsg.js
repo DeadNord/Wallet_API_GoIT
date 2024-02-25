944e8c67445e027c8671aa1c0297620e0b9570af
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-02-25 20:56:57 +0100

    fix mailer and node version

--- src/services/mailer/SendMsg.js
+++ src/services/mailer/SendMsg.js
@@ -1,17 +1,42 @@
-const sgMail = require('@sendgrid/mail');
+const { MAIL_API_KEY } = process.env;
+const { MAIL_SECRET_KEY } = process.env;
 
-const { SENDGRID_API_KEY } = process.env;
+const Mailjet = require('node-mailjet');
+
+const mailjet = Mailjet.apiConnect(MAIL_API_KEY, MAIL_SECRET_KEY, {
+  config: {},
+  options: {},
+});
+
 const VerificationEmail = process.env.EMAIL;
 
-sgMail.setApiKey(SENDGRID_API_KEY);
-
 const SendMsg = async data => {
-  const msg = { from: VerificationEmail, ...data };
+  const request = mailjet.post('send', { version: 'v3.1' }).request({
+    Messages: [
+      {
+        From: {
+          Email: VerificationEmail,
+          Name: VerificationEmail,
+        },
+        To: [
+          {
+            Email: data.to,
+          },
+        ],
+        Subject: data.subject,
+        TextPart: data.text,
+        HTMLPart: data.html || '',
+      },
+    ],
+  });
+
   try {
-    await sgMail.send(msg);
+    const result = await request;
+    console.log(result.body);
     return true;
   } catch (error) {
-    console.log(error.message);
+    console.log(error.statusCode);
+    return false;
   }
 };
 
